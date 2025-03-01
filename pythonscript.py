bcb9839931969ed3d043bc1a3c8c107978a58841
vagrant <vagrant@hellocloud-native-box>
2025-03-01 11:56:45 +0000

    HelloDocker

---
+++ pythonscript.py
@@ -0,0 +1,6 @@
+import pyfiglet
+print("hello CNFP4 from python script")
+art_text = "Hello Docker"
+art_font = "slant"
+ascii_art = pyfiglet.figlet_format(art_text, font = art_font)
+print(ascii_art)
